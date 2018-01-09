f8ea87ef935dcf4eb354f6557e9744d388aaa4a8
pragatipriya25 <35263125+pragatipriya25@users.noreply.github.com>
2018-01-09 19:20:57 +0530

    Create negorze.c

---
+++ negorze.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+void main()
+{
+int n,i;
+printf("enter the number");
+scanf("%d",&n);
+if(n>=1)
+{
+  printf("Positive");
+}
+else if(n==0)
+{
+  printf("Zero");
+}
+else
+{
+  printf("Negative");
+}
+}
