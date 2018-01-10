3577cb25586a078bc579e89879352549011b4ae5
pragatipriya25 <35263125+pragatipriya25@users.noreply.github.com>
2018-01-10 07:44:06 +0530

    Create alphaornot.c

---
+++ alphaornot.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+void main()
+{
+char a;
+int val=int(char a);
+if(val>=65 && val<=90)
+printf("Alphabet");
+else if(val>=97 && val<=122)
+printf("Alphabet");
+else
+printf("No");
+}
