86ebc6e29268b27edd528781f71101963e375063
pragatipriya25 <35263125+pragatipriya25@users.noreply.github.com>
2018-01-11 21:47:26 +0530

    Rename sum.c to sum11.c

---
+++ sum11.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+void main()
+{
+	int a[10],n,k,sum=0,i;
+	printf("enter n");
+	scanf("%d",&n);
+	printf("enter k");
+	scanf("%d",&k);
+	printf("enter values");
+	for(i=0;i<=n;i++)
+	{
+	scanf("%d",&a[i]);
+}
+	for(i=0;i<k;i++){
+	sum=sum+a[i];
+}
+	printf ("%d",sum);
+	
+}
